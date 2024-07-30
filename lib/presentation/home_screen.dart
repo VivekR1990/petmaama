62db8e9992fdba035faf3fdb9e037a91a77f704a
abhinands <abhinands488@gmail.com>
2024-07-30 15:45:03 +0530

    added

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pet_care/presentation/map%20function/mapping.dart';
 
+import 'package:permission_handler/permission_handler.dart';
 
 ValueNotifier<bool> scrollNotifier = ValueNotifier(true);
 int randomIndex = 0;
+
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,17 +17,37 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    PermissionStatus status = await Permission.location.request();
+    if (status.isGranted) {
+      // Permission is granted, proceed with displaying the map
+    } else if (status.isDenied || status.isPermanentlyDenied) {
+      // Permission denied, handle appropriately
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar:AppBar(
-          leading: const Icon(Icons.location_on_outlined),
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LocationScreen()),
+              );
+            },
+            child: const Icon(Icons.location_on_outlined),
+          ),
           actions: const [Icon(Icons.shopping_bag_outlined)],
-          
         ),
       ),
     );
   }
-
-  
 }
