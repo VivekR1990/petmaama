e2320cce7c0f60bd0b13c81edb72d1863645360d
= <=>
2024-08-28 18:04:25 +0530

    service page

--- lib/presentation/service_page.dart
+++ lib/presentation/service_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-i<<<<<<< HEAD
-import 'package:pet_care/design%20v2/loginpage.dart';
-=======
->>>>>>> b249a3b91729a634dbaf63a9cc78171d042747e3
+import 'package:pet_care/loginpage.dart';
 
 ValueNotifier<bool> scrollNotifier = ValueNotifier(true);
-int randomIndex = 1;
+int randomIndex = 3;
 
 class ServicePage extends StatefulWidget {
   const ServicePage({super.key});
@@ -29,7 +26,9 @@ class _ServicePageState extends State<ServicePage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height:8,),
+                SizedBox(
+                  height: 8,
+                ),
                 const Text(
                   "Grooming",
                   style: TextStyle(
@@ -38,7 +37,9 @@ class _ServicePageState extends State<ServicePage> {
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                SizedBox(height: 8,),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -50,9 +51,9 @@ class _ServicePageState extends State<ServicePage> {
                         "https://cdn.pixabay.com/photo/2016/10/10/14/46/icon-1728549_1280.jpg"),
                   ],
                 ),
-
-                 
-                SizedBox(height: 8,),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -64,8 +65,9 @@ class _ServicePageState extends State<ServicePage> {
                         "https://cdn.pixabay.com/photo/2016/10/10/14/46/icon-1728549_1280.jpg"),
                   ],
                 ),
-
-                 SizedBox(height:16,),
+                SizedBox(
+                  height: 16,
+                ),
                 const Text(
                   "Pet Food",
                   style: TextStyle(
@@ -74,7 +76,9 @@ class _ServicePageState extends State<ServicePage> {
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                SizedBox(height: 8,),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -86,8 +90,9 @@ class _ServicePageState extends State<ServicePage> {
                         "https://cdn.pixabay.com/photo/2016/10/10/14/46/icon-1728549_1280.jpg"),
                   ],
                 ),
-
-                  SizedBox(height: 8,),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -99,10 +104,9 @@ class _ServicePageState extends State<ServicePage> {
                         "https://cdn.pixabay.com/photo/2016/10/10/14/46/icon-1728549_1280.jpg"),
                   ],
                 ),
-
-
-
-                  SizedBox(height:16,),
+                SizedBox(
+                  height: 16,
+                ),
                 const Text(
                   "Accessories",
                   style: TextStyle(
@@ -111,7 +115,9 @@ class _ServicePageState extends State<ServicePage> {
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                SizedBox(height: 8,),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -130,107 +136,110 @@ class _ServicePageState extends State<ServicePage> {
       ),
     );
   }
+}
 
-  Widget imageWithLabel(BuildContext context, String name, String imageUrl) {
-    return GestureDetector(
-      onTap: () {
-        // Navigate to the corresponding page based on the name
-        switch (name) {
-          case 'Bathing & Drying':
-            
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Haircut & Styling':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Nail Trimming':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Ear Cleaning':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Teeth Brushing':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Flea & Tick Treatment':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Kibble':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Canned Food':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Semi-Moist Dog Food':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Dry Pet Food':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Raw Dog Food':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-          case 'Wet Pet Food':
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-            break;
-        }
-      },
-      child: Container(
-        padding: const EdgeInsets.all(2.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Color.fromRGBO(255,191,0,1),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
+Widget imageWithLabel(BuildContext context, String name, String imageUrl) {
+  return GestureDetector(
+    onTap: () {
+      // Navigate to the corresponding page based on the name
+      switch (name) {
+        case 'Bathing & Drying':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Haircut & Styling':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Nail Trimming':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Ear Cleaning':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Teeth Brushing':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Flea & Tick Treatment':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Kibble':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Canned Food':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Semi-Moist Dog Food':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Dry Pet Food':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Raw Dog Food':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+        case 'Wet Pet Food':
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+          break;
+      }
+    },
+    child: Container(
+      padding: const EdgeInsets.all(2.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color.fromRGBO(255, 191, 0, 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
             ),
-          ],
-        ),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ),
-              child: Image.network(
-                imageUrl,
-                height: 100,
-                width: 100,
-                fit: BoxFit.cover,
-              ),
+            child: Image.network(
+              imageUrl,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
             ),
-            Container(
-              alignment: Alignment.center,
-              height: 50,
-              width: 100, // Match the width of the image
-              child: Text(
-                name,
-                textAlign: TextAlign.center, // Center align the text
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                maxLines: 2, // Optional: set a maximum number of lines
-                overflow: TextOverflow
-                    .ellipsis, // Optional: handle overflow with ellipsis
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 50,
+            width: 100, // Match the width of the image
+            child: Text(
+              name,
+              textAlign: TextAlign.center, // Center align the text
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
+              maxLines: 2, // Optional: set a maximum number of lines
+              overflow: TextOverflow
+                  .ellipsis, // Optional: handle overflow with ellipsis
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
